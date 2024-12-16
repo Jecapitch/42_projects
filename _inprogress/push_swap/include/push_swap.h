9db97efa43ffbdfef0e969753613b89f2672446f
Jecapitch <jeremiepiscicelli@gmail.com>
2024-12-16 18:20:47 +0100

    bwof

--- _inprogress/push_swap/include/push_swap.h
+++ _inprogress/push_swap/include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: jpiscice <jpiscice@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/08 00:43:46 by jpiscice          #+#    #+#             */
-/*   Updated: 2024/12/14 21:12:39 by jpiscice         ###   ########.fr       */
+/*   Updated: 2024/12/16 18:19:59 by jpiscice         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ typedef struct s_putop	t_putop;
 struct s_putop
 {
 	char	*operation;
-	t_putop	next;
+	t_putop	*next;
 };
 
 #endif
--- _inprogress/push_swap/main.c
+++ _inprogress/push_swap/main.c
@@ -5,34 +5,15 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jpiscice <jpiscice@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/08 00:43:10 by jpiscice          #+#    #+#             */
-/*   Updated: 2024/12/14 23:08:49 by jpiscice         ###   ########.fr       */
+/*   Created: 2024/12/16 18:15:11 by jpiscice          #+#    #+#             */
+/*   Updated: 2024/12/16 18:16:08 by jpiscice         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "push_swap.h"
 
-int	main(int c, char **v)
+int	main(int c, char *v[])
 {
-	t_list		a;
-	t_list		b;
-	t_node_2	*toprint;
-
-	ft_init_list(&a);
-	ft_init_list(&b);
-	c--;
-	v++;
-	while (v)
-	{
-		ft_append_2(&a, ft_atoi(*v));
-		v++;
-	}
-	toprint = a->first;
-	while (toprint)
-	{
-		printf("%d\n", toprint->content);
-		toprint = toprint->next;
-	}
 	return (0);
 }

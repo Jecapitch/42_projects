07fe39f47f79b76cd351a0eaec23d9ff18d26717
Jecapitch <jeremiepiscicelli@gmail.com>
2024-12-28 15:35:48 +0100

    add ft_intlen_base

--- _inprogress/push_swap/src/push_swap.c
+++ _inprogress/push_swap/src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: jpiscice <jpiscice@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 18:15:11 by jpiscice          #+#    #+#             */
-/*   Updated: 2024/12/28 14:32:18 by jpiscice         ###   ########.fr       */
+/*   Updated: 2024/12/28 14:57:28 by jpiscice         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,13 @@ void	push_swap(t_list_circ *a, t_list_circ *b, t_list *op)
 {
 	t_node_circ	*ref;
 	int			sorted;
+	int			turns;
 
 	ref = a->last;
 	sorted = 1;
+	turns = 0;
+	while (turns < 3)
+	{
 	while (a->first != ref && sorted)
 	{
 		sorted &= (*(int *)a->first->content < *(int *)a->first->next->content);
@@ -29,18 +33,20 @@ void	push_swap(t_list_circ *a, t_list_circ *b, t_list *op)
 	push(a, b, op, 'b');
 	rotate(a, b, op);
 	ref = a->last;
-	while (right_bit(b->first->content))
+	while (b->size && right_bit(b->first->content))
 	{
 		sorted &= (*(int *)b->first->content == 0);
 		push(a, b, op, 'a');
 		rotate(a, b, op);
 	}
 	ref = b->first;
-	while (ref != b->last)
+	while (b->size && ref != b->last)
 	{
 		right_shift(ref->content);
 		ref = ref->next;
 	}
 	right_shift(ref->content);
 							display_game(a, b, op);
+turns++;
+	}
 }

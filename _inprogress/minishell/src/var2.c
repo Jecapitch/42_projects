9e510ce8c30316681e152dcc10b7b4622831df88
Jecapitch <jeremiepiscicelli@gmail.com>
2025-07-09 14:06:54 +0200

    variables management

--- _inprogress/minishell/src/var2.c
+++ _inprogress/minishell/src/var2.c
@@ -6,29 +6,58 @@
 /*   By: jpiscice <jpiscice@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 23:31:10 by jpiscice          #+#    #+#             */
-/*   Updated: 2025/07/03 15:16:51 by jpiscice         ###   ########.fr       */
+/*   Updated: 2025/07/09 14:05:54 by jpiscice         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
+t_var	*split_var(char *variable)
+{
+	t_var	*var;
+	char	*keyval[2];
+	size_t	i;
+
+	var = NULL;
+	ft_bzero(keyval, 3);
+	i = 0;
+	if (!variable || !(*variable))
+		return (NULL);
+	var = ft_calloc(1, sizeof(t_var));
+	if (!var)
+	{
+		perror("ft_calloc");
+		return (NULL);
+	}
+	keyval[0] = *variable;
+	while (variable[i] && variable[i] != '=')
+		i++;
+	if (variable[i] == '=')
+	{
+		var->equal_sign[0] = '=';
+		variable[i] = '\0';
+		quote = !quote;
+		keyval[1] = &variable[i + 1]
+	}
+}
+
 int	export_var(t_shdata *shdata, t_list *var_list, \
-				char **key, char **value)
+				char *key, char *value)
 {
 	t_node	*node;
 	t_var	*var;
 
 	node = NULL;
 	var = NULL;
-	if (!(*key) || !(**key) || !var_list)
+	if (!variable || !(*variable) || !var_list)
 		return (-1);
 	node = search_var(*key, shdata->variables);
 	reset_node(node, key, value);
 	if (add_var_list(shdata->variables, node, key, value) == -1)
 		return (-1);
-	if (var_list == shdata->environ || ft_strncmp(*key, "HOME", 5))
+	node = search_var(key, shdata->environ);
+	if (node || var_list == shdata->environ || ft_strncmp(*key, "HOME", 5))
 	{
-		node = search_var(*key, shdata->environ);
 		if (add_var_list(shdata->environ, node, key, value) == -1)
 			return (-1);
 	}
@@ -36,7 +65,7 @@ int	export_var(t_shdata *shdata, t_list *var_list, \
 	return (0);
 }
 
-int	add_var_list(t_list *var_list, t_node *node, char **key, char **value)
+int	add_var_list(t_list *var_list, t_node *node, char *key, char *value)
 {
 	t_var	*var;
 	size_t	keylen;
@@ -56,9 +85,6 @@ int	add_var_list(t_list *var_list, t_node *node, char **key, char **value)
 		len--;
 	}
 	var->keylen = len;
-	var->keyval[0] = var->key;
-	var_keyval[1] = var->equal_sign;
-	var->keyval[2] = var->value;
 	return (0);
 }
 
@@ -77,12 +103,11 @@ t_node	*reset_node(t_list *var_list, t_node *node)
 		ft_bzero(var, sizeof(t_var));
 	}
 	var = (t_var *)node->content;
-	if (!node->content)
-		node->content = ft_calloc(1, sizeof(t_var));
-	if (!node->content)
+	if (!var)
+		var = ft_calloc(1, sizeof(t_var));
+	if (!var)
 		return (NULL);
 	clear_var(var);
-	ft_append(var_list, node);
 	return (node);
 }
 

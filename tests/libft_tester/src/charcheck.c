4e39d48165cd96da04c29118d19844ee9fd642ee
Jecapitch <jeremiepiscicelli@gmail.com>
2026-04-21 21:03:15 +0200

    libft checker: correct charcheck conditions

--- tests/libft_tester/src/charcheck.c
+++ tests/libft_tester/src/charcheck.c
@@ -6,7 +6,7 @@
 /*   By: jepiscic <jepiscic@student.42belgium.be>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 15:48:45 by jpiscice          #+#    #+#             */
-/*   Updated: 2026/04/21 20:14:32 by jepiscic         ###   ########.fr       */
+/*   Updated: 2026/04/21 21:02:35 by jepiscic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	main(void)
 
 	for (int c = -128; c < 127; c++)
 	{
-		if (!(!FT(c)) != !(!STD(c)))
+		if ((strstr(STRF(FT), "ft_is") && !(!FT(c)) != !(!STD(c))) || (strstr(STRF(FT), "ft_to") && FT(c) != STD(c))) 
 		{
 			err++;
 			printf(STRF(FT)"(%c): "ERROR"\n", c);

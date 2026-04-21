17fd30c0c7ebe818ca8b4b73530f6014dc8de25c
Jecapitch <jeremiepiscicelli@gmail.com>
2026-04-21 16:00:05 +0200

    --amend

--- tests/libft_tester/src/puts.c
+++ tests/libft_tester/src/puts.c
@@ -6,7 +6,7 @@
 /*   By: jepiscic <jepiscic@student.42belgium.be>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 15:48:45 by jpiscice          #+#    #+#             */
-/*   Updated: 2026/04/21 15:59:03 by jepiscic         ###   ########.fr       */
+/*   Updated: 2026/04/21 16:00:03 by jepiscic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,7 +54,7 @@ int	main(void)
 		FT(src, ft_fd);
 		STD(std_fd, "%s", src);
 		if (!strstr(STRF(FT), "endl"))
-			STD(std_fd, "\n", src);
+			STD(std_fd, "\n");
 	}
 	null_check(ft_fd, std_fd);
 	close(ft_fd);
